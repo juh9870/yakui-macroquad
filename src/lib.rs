76acfb70f4d208d04e109ae0c1f8e058625967a4
juh9870 <juh9870@ukr.net>
2024-05-09 01:53:25 +0300

    feat: user textures accessor

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,7 @@ use macroquad::window::get_internal_gl;
 use yakui_miniquad::*;
 
 pub use macroquad;
+use yakui_miniquad::miniquad::TextureId;
 
 struct Yakui(YakuiMiniQuad, usize);
 
@@ -138,6 +139,12 @@ pub fn ui<F: FnOnce(&mut yakui_core::Yakui)>(f: F) {
 /// Allows you configure the yakui context within the scope of the closure passed, if you need to.
 pub fn cfg<F: FnOnce(&mut yakui_core::Yakui)>(f: F) {
     f(get_yakui().as_mut().unwrap().0.ctx());
+}
+
+/// Gives you direct access to the vector of user textures managed by
+/// [YakuiMiniQuad] within the scope of the closure passed
+pub fn user_textures<F: FnOnce(&mut Vec<TextureId>) -> R, R>(f: F) -> R {
+    f(get_yakui().0.user_textures())
 }
 
 /// Draws the yakui ui. Must be called after `finish`/`ui` and once per frame.
